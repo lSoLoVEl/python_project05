ee6575f7c067f128de5190fd88e6335eaa69ebe3
lSoLoVEl <S6519410044@sau.ac.th>
2023-09-15 12:16:25 +0700

    Code v3:CalNetsalary

--- test05.py
+++ test05.py
@@ -13,3 +13,38 @@
 #ภาษีเป็นเงิน : <output บาท (ของทศนิยม 2 ตำแหน่ง)
 #เงินเดือนสุทธิเป็นเงิน : <output> บาท (ขอทศนิยม 2 ตำแหน่ง)
 #=============================================
+
+#1.รับค่าข้อมูลจากพนักงาน 2.คำนวณเงินเดือนพนักงานสุทธิ 3.คำนวณหาค่าภาษีของพนักงาน 4.                 5.แสดงผล
+
+def inputData( ) :
+    id = str(input( 'โปรดป้อนรหัสพนักงาน : ' ))
+    name = str(input( 'โปรดป้อนชื่อพนักงาน : '))
+    salary = float(input( 'โปรดป้อนเงินเดือน : '))
+    return id,name,salary
+
+def calTax(salary ) :
+    tax = salary*(7/100)
+    return tax
+
+def socialSecurity( ) :
+    socialSecurity = 500
+    return socialSecurity
+
+def calNetsalary(salary,tax,socialSecurity ) :
+    netsalary = salary - tax- socialSecurity
+    return netsalary
+
+def showSalaryTaxSocialSecurityAndNetsalary(salary,tax,socialSecurity,netsalary) :
+    print(f'ภาษีเป็นเงิน {tax:.2f} บาท')
+    print(f'เบี้ยประกันสังคม {socialSecurity:.2f} บาท')
+    print(f'เงินเดือนสุทธิเป็นเงิน {netsalary:.2f} บาท')
+    
+print('==========================================================')
+print('                   คำนวณเงินเดือนพนักงาน               ')
+print('==========================================================')
+id,name,salary = inputData( )
+tax = calTax( salary)
+socialSecurity = socialSecurity( )
+netsalary = calNetsalary(salary,tax,socialSecurity )
+showSalaryTaxSocialSecurityAndNetsalary(salary,tax,socialSecurity,netsalary)
+print('==========================================================')
